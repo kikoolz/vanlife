30386216d0e1429f3ab5f0a87afd13f594243be8
kikoolz <kenlubs45@gmail.com>
2025-05-25 10:26:11 +0400

    Implement shared layout with Header and Footer; enhance navigation with active route styling

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import VanDetail from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
 import "./index.css";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
@@ -30,6 +31,7 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
           </Route>
         </Route>
       </Routes>
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HostVans() {
+  return <h1>HostVans</h1>;
+}
