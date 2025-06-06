e1bc5702d9b90d7f8f73e5cf256fa791f8707d94
kikoolz <kenlubs45@gmail.com>
2025-06-06 04:37:16 +0400

    feat: Add Error component with detailed error display
    - Add useRouteError hook for accessing error details
    - Display error message, status and stack trace
    - Add error container styling
    - Implement custom error formatting
    - Add console logging for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import "./index.css";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
 import NotFound from "./pages/NotFound";
+import Error from "./components/Error";
 import { loader as vansLoader } from "./pages/Vans/Vans";
 import "./server";
 
@@ -31,7 +32,12 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
 
       <Route path="vans">
-        <Route index element={<Vans />} loader={vansLoader} />
+        <Route
+          index
+          element={<Vans />}
+          errorElement={<Error />}
+          loader={vansLoader}
+        />
         <Route path=":id" element={<VanDetail />} />
       </Route>
 
---
+++ src/components/Error.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+export default function Error() {
+  const error = useRouteError();
+  console.log();
+  return (
+    <div className="error-container">
+      <h1>Error: {error.message}</h1>
+      <p>
+        {error.statusText} - {error.status}
+      </p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
+}
